2feb55dfed2e72562b612ca44fdbcb5df593733f
axel.roy <axel.roy@he-arc.ch>
2016-05-31 16:05:38 +0200

    Ajout de la classe de définition des Poll et classe de gestion des Poll

---
+++ poll.py
@@ -0,0 +1,77 @@
+from collections import Counter
+
+
+#=============================================================
+#   CLASSES
+#=============================================================
+
+
+class Poll():
+    """
+    Manage a simple poll.
+
+    The life cycle of a poll is the follow:
+    1) Not started : can set the questions, the choices and start the poll
+    2) In progress : can answer to the poll and close the poll
+    3) Closed : answers are closed, can get the result
+    """
+
+    def __init__(self, title):
+        self._title = title
+        self._choices = []
+        self._answers = {}
+        self._closed = False
+        self._started = False
+        self._question = ""
+
+    def set_question(self, question):
+        if not self._started:
+            self._question = question
+            return True
+        return False
+
+    def get_question(self):
+        return self._question
+
+    def set_choices(self, choices_list):
+        if not self._started :
+            # force all value to be unique
+            choices_list = set(choices_list)
+            self._choices = list(choices_list)
+            return True
+        return False
+
+    def get_choices(self):
+        return self._choices
+
+    def set_answer(self, user_name, number):
+        if not self._closed and self._started:
+            if number in range(1, len(self._choices)+1):
+                self._answers[user_name] = number
+                return True
+        return False
+
+    def get_answer(self, user_name):
+        pass
+
+    def close(self):
+        if self._started:
+            self._closed = True
+            return True
+        return False
+
+    def is_closed(self):
+        return self._closed
+
+    def start(self):
+        if len(self._choices) >= 2:
+            self._started = True
+            return True
+        return False
+
+    def is_started(self):
+        return self._started
+
+    def get_result(self):
+        c = Counter(self._answers.values())
+        return [(self._choices[x[0]-1],x[1])  for x in c.most_common()];
---
+++ pollmanager.py
@@ -0,0 +1,178 @@
+
+#=============================================================
+#   IMPORTS
+#=============================================================
+
+
+from poll import Poll
+
+
+#=============================================================
+#   DECORATORS
+#=============================================================
+
+
+def check_user_decorator(f):
+    def _f(self, title, user, **kwargs):
+        owner = self.poll_list.get(title, None)[0]
+
+        if owner != user:
+            return False, "You have not the right to edit this poll"
+
+        return f(self, title, user, **kwargs)
+
+    return _f
+
+
+def check_poll_exist_decorator(f):
+    def _f(self, title, user, **kwargs):
+        user_poll = self.poll_list.get(title, None)
+
+        if not user_poll:
+            return False, "The poll \""+title+"\" does not exist"
+
+        return f(self, title, user, **kwargs)
+
+    return _f
+
+
+#=============================================================
+#   TOOLS
+#=============================================================
+
+
+def _create_choices_string(poll):
+    i = 1
+    l = []
+    for c in poll.get_choices():
+        l.append("\n  "+str(i)+") "+c)
+        i += 1
+    return "".join(l)
+
+
+def _create_result_string(poll):
+    i = 1
+    l = []
+    for r in poll.get_result():
+        l.append("\n  "+str(i)+") "+r[0]+" (x"+str(r[1])+")")
+        i += 1
+    return "".join(l)
+
+
+#=============================================================
+#   CLASSES
+#=============================================================
+
+
+class PollManager():
+    """
+    Manage the the poll relative to user.
+
+    All methods provide a output with a boolean (if the method succeded) and
+    a string (which describe the error or the success)
+    """
+
+    def __init__(self):
+        self.poll_list = {}
+
+    def help(self):
+        return True, "[command] [poll] [arg]"
+
+    def create_poll(self, title, user, **kwargs):
+        if title in self.poll_list:
+            return False, "The poll already exist, remove it before"
+
+        question = kwargs.get("question", "")
+        choices = kwargs.get("choices", [])
+
+        poll = (user, Poll(title))
+        self.poll_list[title] = poll
+
+        if question:
+            poll.set_question(question)
+        if choices:
+            poll.set_choices(choices)
+
+        return True, "The poll \""+title+"\" has been created"
+
+    @check_poll_exist_decorator
+    @check_user_decorator
+    def remove_poll(self, title, user, **kwargs):
+        self.poll_list.pop(title, None)
+
+        return True, "The poll \""+title+"\" has been deleted"
+
+
+    @check_poll_exist_decorator
+    @check_user_decorator
+    def set_question(self, title, user, **kwargs):
+        poll = self.poll_list.get(title, None)[1]
+
+        poll.set_question(kwargs["question"])
+
+        return True, "The question has been update"
+
+
+    @check_poll_exist_decorator
+    @check_user_decorator
+    def set_choices(self, title, user, **kwargs):
+        poll = self.poll_list.get(title, None)[1]
+
+        poll.set_choices(kwargs["choices"])
+
+        return True, "The choices have been added"
+
+
+    @check_poll_exist_decorator
+    def answer_poll(self, title, user, **kwargs):
+        poll = self.poll_list.get(title, None)[1]
+
+        try:
+            number = int(kwargs["answer"])
+
+            if poll.set_answer(user, number):
+                return True, "Your answer has been registered"
+            else:
+                return False, "You can not answer to poll not started or closed"
+        except ValueError:
+            return False, "You must enter the number of the choice :" + _create_choices_string(poll)
+
+    @check_poll_exist_decorator
+    @check_user_decorator
+    def close_poll(self, title, user, **kwargs):
+        poll = self.poll_list.get(title, None)[1]
+
+        if poll.close():
+            return True, "The poll \""+title+"\" has been closed"
+        else:
+            return False, "Can not close a poll which is not started"
+
+
+    @check_poll_exist_decorator
+    @check_user_decorator
+    def start_poll(self, title, user, **kwargs):
+        poll = self.poll_list.get(title, None)[1]
+
+        if poll.start():
+            return True, "The poll \""+title+"\" has been started"
+        else:
+            return False, "Not enough choices for start"
+
+
+    @check_poll_exist_decorator
+    def show_poll(self, title, user, **kwargs):
+        poll = self.poll_list.get(title, None)[1]
+
+        show = []
+        show.append("Poll : ")
+        show.append(title)
+        show.append("\n - Question : ")
+        show.append(poll.get_question())
+        show.append("\n - Choices : ")
+        show.append(_create_choices_string(poll))
+        show.append("\n - Result : ")
+        show.append(_create_result_string(poll))
+        show.append("\n - Status : ")
+        show.append("Not start" if not poll.is_started() else "In progress" if not poll.is_closed() else "Closed")
+
+        return True, "".join(show)
